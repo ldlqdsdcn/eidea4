2ad0471597b41b03f453fe3c3d16e1993e6a9951
solomon.liu <ldlqdsdcn@gmail.com>
2017-04-13 17:43:11 +0800

    添加swagger-ui2

--- eapi/src/main/java/com/dsdl/eidea/base/api/ClientApi.java
+++ eapi/src/main/java/com/dsdl/eidea/base/api/ClientApi.java
@@ -3,8 +3,8 @@ package com.dsdl.eidea.base.api;
 import com.dsdl.eidea.api.model.Client;
 import com.dsdl.eidea.core.web.result.ApiResult;
 import com.google.common.reflect.TypeToken;
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +18,7 @@ import java.util.Date;
 @RestController
 @Api(value = "Client", position = 0,description = "实体信息",consumes="application/json")
 public class ClientApi {
+    public static final Class RETURN_CLASS=new TypeToken<ApiResult<String>>(){}.getClass();
     @ApiOperation(value = "根据实体ID获取实体", httpMethod = "GET", response = Client.class, notes = "根据实体的id信息获取实体的详情",produces = MediaType.APPLICATION_JSON_VALUE)
     @RequestMapping("/client/{id}")
     public Client getClient(@PathVariable("id") Integer id)
@@ -34,7 +35,7 @@ public class ClientApi {
     @RequestMapping("/client/save")
     public ApiResult<Client> saveClient(Client client)
     {
-        new TypeToken<ApiResult<String>>(){}.getClass();
+
         return ApiResult.success(new Client());
     }
 }

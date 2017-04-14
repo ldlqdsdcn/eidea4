bc34ae4fdb73337c639de81c2e0ebf83ca4e614f
solomon.liu <ldlqdsdcn@gmail.com>
2017-04-14 16:56:29 +0800

    清理掉一些没用的类

--- ebase/src/main/java/com/dsdl/eidea/base/entity/bo/UserSessionBo.java
+++ ebase/src/main/java/com/dsdl/eidea/base/entity/bo/UserSessionBo.java
@@ -1,6 +1,5 @@
 package com.dsdl.eidea.base.entity.bo;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,9 +10,7 @@ import java.util.Date;
 public class UserSessionBo implements java.io.Serializable{
 	private Integer id;
 	private String sessionId;
-	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
 	private Date loginDate;
-	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
 	private Date logoutDate;
 	private Integer userId;
 	private String remoteAddr;

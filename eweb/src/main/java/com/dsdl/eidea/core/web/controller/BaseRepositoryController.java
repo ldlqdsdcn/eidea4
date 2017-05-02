94a5dbe26c489c85b915a9a021311d858362929c
solomon.liu <ldlqdsdcn@gmail.com>
2017-05-02 08:26:34 +0800

    去掉无用的Controller类

---
+++ eweb/src/main/java/com/dsdl/eidea/core/web/controller/BaseRepositoryController.java
@@ -0,0 +1,14 @@
+package com.dsdl.eidea.core.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by 刘大磊 on 2017/4/28 14:09.
+ */
+public class BaseRepositoryController extends BaseController {
+    @RequestMapping("/export")
+    public void export(String type)
+    {
+
+    }
+}
--- eweb/src/main/java/com/dsdl/eidea/core/web/def/WebConst.java
+++ eweb/src/main/java/com/dsdl/eidea/core/web/def/WebConst.java
@@ -4,20 +4,42 @@ package com.dsdl.eidea.core.web.def;
  * Created by 刘大磊 on 2016/12/20 17:03.
  */
 public class WebConst {
-    public static final String SESSION_LOGINUSER="loginUser";
-    public static final String PAGING_SETTINGS="pagingSettingResult";
-    public static final String SESSION_USERCONTENT="userContent";
-    public static final String SESSION_SEARCH_PARAM="_searchParam";
-    public static final String PAGE_URI="uri";
-    public static final String SESSION_ACTUALUSER="actualUser";
-    public static final String SESSION_LEFTMENU="menuString";
-    public static final String PAGE_PRIVILEGES="pagePrivileges";
-    public static final String PRIVILEGES="privileges";
-    public static final String SESSION_RESOURCE="userResource";
-    public static final String ORG_IDS="orgIdList";
+    public static final String SESSION_LOGINUSER = "loginUser";
+    public static final String PAGING_SETTINGS = "pagingSettingResult";
+    public static final String SESSION_USERCONTENT = "userContent";
+    public static final String SESSION_SEARCH_PARAM = "_searchParam";
+    public static final String PAGE_URI = "uri";
+    public static final String SESSION_ACTUALUSER = "actualUser";
+    public static final String SESSION_LEFTMENU = "menuString";
+    public static final String PAGE_PRIVILEGES = "pagePrivileges";
+    public static final String PRIVILEGES = "privileges";
+    public static final String SESSION_RESOURCE = "userResource";
+    public static final String ORG_IDS = "orgIdList";
     /**
      * token header 权限验证
      */
-    public static final String HEADER_TOKEN="token";
-
+    public static final String HEADER_TOKEN = "token";
+    /*****************************************************
+     * 常用占位符
+     */
+    /**
+     * 当前用户的OrgId
+     */
+    public static final String EXPRESS_ORG_ID = "@UserOrgId@";
+    /**
+     * 当前用户的clientId
+     */
+    public static final String EXPRESS_CLIENT_ID = "@UserClientId@";
+    /**
+     * 当前用户的 userId
+     */
+    public static final String EXPRESS_USER_ID = "@UserId@";
+    /**
+     * 当前时间
+     */
+    public static final String EXPRESS_CURRENT_TIME = "@CurrentTime@";
+    /**
+     * 当前日期
+     */
+    public static final String EXPRESS_CURRENT_DATE = "@CurrentDate@";
 }

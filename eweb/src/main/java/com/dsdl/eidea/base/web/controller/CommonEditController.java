3b1210c8a8d469b6d3197673d3d2109ce80912b3
solomon.liu <ldlqdsdcn@gmail.com>
2017-05-03 14:13:21 +0800

    添加公共编辑功能

---
+++ eweb/src/main/java/com/dsdl/eidea/base/web/controller/CommonEditController.java
@@ -0,0 +1,13 @@
+package com.dsdl.eidea.base.web.controller;
+
+import com.dsdl.eidea.base.service.WindowService;
+
+/**
+ * Created by 刘大磊 on 2017/5/3 9:23.
+ * 公共编辑类
+ * 增 删 改 查 导入 导出 基本命令
+ */
+public final class CommonEditController {
+    private WindowService windowService;
+
+}

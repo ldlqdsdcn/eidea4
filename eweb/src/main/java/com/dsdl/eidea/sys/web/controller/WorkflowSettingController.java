40def0017f2f4b90e42bce5c978a4a207207720f
solomon.liu <ldlqdsdcn@gmail.com>
2017-05-17 14:05:04 +0800

    activiti提交部分代码，添加在线部署功能。

--- eweb/src/main/java/com/dsdl/eidea/sys/web/controller/WorkflowSettingController.java
+++ eweb/src/main/java/com/dsdl/eidea/sys/web/controller/WorkflowSettingController.java
@@ -1,23 +1,32 @@
 package com.dsdl.eidea.sys.web.controller;
 
-import com.dsdl.eidea.base.entity.po.FileSettingPo;
 import com.dsdl.eidea.base.service.FileSettingService;
+import com.dsdl.eidea.core.web.def.WebConst;
 import com.dsdl.eidea.core.web.result.JsonResult;
-import com.dsdl.eidea.core.web.result.def.ErrorCodes;
+import com.dsdl.eidea.core.web.vo.PagingSettingResult;
+import com.dsdl.eidea.sys.web.vo.ProcessDefinitionVo;
 import lombok.extern.slf4j.Slf4j;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.repository.ProcessDefinitionQuery;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipInputStream;
 
 /**
  * Created by Bobo on 2017/5/12.
@@ -27,42 +36,158 @@ import java.util.UUID;
 @Slf4j
 public class WorkflowSettingController {
     @Autowired
-    private FileSettingService fileSettingService;
+    private RuntimeService runtimeService;
+    @Autowired
+    private RepositoryService repositoryService;
+
     @RequestMapping(value = "/showList", method = RequestMethod.GET)
-    public ModelAndView list() {
+    @RequiresPermissions("view")
+    public ModelAndView showList() {
         ModelAndView modelAndView = new ModelAndView("/sys/workflow/workflow");
+        modelAndView.addObject(WebConst.PAGING_SETTINGS, PagingSettingResult.getDefault());
         return modelAndView;
     }
 
-    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    /**
+     * 查看已部署的工作流程
+     *
+     * @return
+     */
+    @RequiresPermissions("view")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
     @ResponseBody
-    public JsonResult<Map<String,Object>> upload(MultipartFile file, HttpServletRequest request){
+    public JsonResult<List<ProcessDefinitionVo>> list() {
+    /*
+     * 保存两个对象，一个是ProcessDefinition（流程定义），一个是Deployment（流程部署）
+     */
+        List<ProcessDefinitionVo> processDefinitionVoList = new ArrayList<>();
+        ProcessDefinitionQuery processDefinitionQuery = repositoryService.createProcessDefinitionQuery().orderByDeploymentId().desc();
+        List<ProcessDefinition> processDefinitionList = processDefinitionQuery.list();
+        for (ProcessDefinition processDefinition : processDefinitionList) {
+            String deploymentId = processDefinition.getDeploymentId();
+            Deployment deployment = repositoryService.createDeploymentQuery().deploymentId(deploymentId).singleResult();
+            ProcessDefinitionVo processDefinitionVo = new ProcessDefinitionVo();
+            processDefinitionVo.setId(processDefinition.getId());
+            processDefinitionVo.setKey(processDefinition.getKey());
+            processDefinitionVo.setName(processDefinition.getName());
+            processDefinitionVo.setDeploymentId(processDefinition.getDeploymentId());
+            processDefinitionVo.setDeploymentTime(deployment.getDeploymentTime());
+            processDefinitionVo.setResourceName(processDefinition.getResourceName());
+            processDefinitionVo.setSuspended(processDefinition.isSuspended());
+            processDefinitionVo.setVersion(processDefinition.getVersion());
 
-        FileSettingPo fileSettingPo=fileSettingService.getFileSettingsByRequestPath(request.getServletPath());
-        if(fileSettingPo==null)
+            processDefinitionVoList.add(processDefinitionVo);
+        }
+        return JsonResult.success(processDefinitionVoList);
+    }
+
+    @RequiresPermissions("delete")
+    @RequestMapping(value = "/deletes", method = RequestMethod.POST)
+    @ResponseBody
+    public JsonResult<List<ProcessDefinitionVo>> deletes(@RequestBody String[] ids)
+    {
+        for(String id:ids)
         {
-            log.error("根据访问路径："+request.getServletPath()+"取不到管理的文件设置");
-            JsonResult.fail(ErrorCodes.BUSINESS_EXCEPTION.getCode(),"文件设置异常，保存失败");
+            repositoryService.deleteDeployment(id, true);
         }
-        try{
-            File fileDirectory=new File(fileSettingPo.getRootDirectory());
-            if(!fileDirectory.exists())
-            {
-                fileDirectory.mkdirs();
+        return list();
+    }
+    /**
+     * 部署工作流到服务器
+     * @param file
+     * @return
+     */
+    @RequiresPermissions("add")
+    @RequestMapping(value = "/deploy", method = RequestMethod.POST)
+    @ResponseBody
+    public JsonResult<Void> deploy(MultipartFile file) {
+
+        String fileName = file.getOriginalFilename();
+        try {
+            InputStream fileInputStream = file.getInputStream();
+            Deployment deployment = null;
+
+            String extension = FilenameUtils.getExtension(fileName);
+            if (extension.equals("zip") || extension.equals("bar")) {
+                ZipInputStream zip = new ZipInputStream(fileInputStream);
+                deployment = repositoryService.createDeployment().addZipInputStream(zip).deploy();
+            } else {
+                deployment = repositoryService.createDeployment().addInputStream(fileName, fileInputStream).deploy();
             }
-            InputStream in=file.getInputStream();
-            FileOutputStream fos = new FileOutputStream(new File(fileDirectory, UUID.randomUUID().toString()));
-            byte[] b = new byte[Integer.parseInt(file.getSize()+"")];
-            int len=0;
-            while((len=in.read(b))!= -1){
-                fos.write(b,0,len);
-            }
-            in.close();
-            fos.close();
-        }catch (IOException io){
-            io.printStackTrace();
-        }finally {
-            return JsonResult.success(new HashMap<String,Object>());
+            repositoryService.createProcessDefinitionQuery().deploymentId(deployment.getId()).list();
+        } catch (Exception e) {
+            log.error("error on deploy process, because of file input stream", e);
         }
+        return JsonResult.success(null);
+    }
+    /**
+     * 读取资源，通过部署ID
+     *
+     * @param processDefinitionId 流程定义
+     * @param resourceType        资源类型(xml|image)
+     * @throws Exception
+     */
+    @RequestMapping(value = "/resource/read")
+    public void loadByDeployment(@RequestParam("processDefinitionId") String processDefinitionId, @RequestParam("resourceType") String resourceType,
+                                 HttpServletResponse response) throws Exception {
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().processDefinitionId(processDefinitionId).singleResult();
+        String resourceName = "";
+        if (resourceType.equals("image")) {
+            resourceName = processDefinition.getDiagramResourceName();
+        } else if (resourceType.equals("xml")) {
+            resourceName = processDefinition.getResourceName();
+        }
+        InputStream resourceAsStream = repositoryService.getResourceAsStream(processDefinition.getDeploymentId(), resourceName);
+        byte[] b = new byte[1024];
+        int len = -1;
+        while ((len = resourceAsStream.read(b, 0, 1024)) != -1) {
+            response.getOutputStream().write(b, 0, len);
+        }
+    }
+
+    /**
+     * 读取资源，通过流程ID
+     *
+     * @param resourceType      资源类型(xml|image)
+     * @param processInstanceId 流程实例ID
+     * @param response
+     * @throws Exception
+     */
+    @RequestMapping(value = "/resource/process-instance")
+    public void loadByProcessInstance(@RequestParam("type") String resourceType, @RequestParam("pid") String processInstanceId, HttpServletResponse response)
+            throws Exception {
+        InputStream resourceAsStream = null;
+        ProcessInstance processInstance = runtimeService.createProcessInstanceQuery().processInstanceId(processInstanceId).singleResult();
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().processDefinitionId(processInstance.getProcessDefinitionId())
+                .singleResult();
+
+        String resourceName = "";
+        if (resourceType.equals("image")) {
+            resourceName = processDefinition.getDiagramResourceName();
+        } else if (resourceType.equals("xml")) {
+            resourceName = processDefinition.getResourceName();
+        }
+        resourceAsStream = repositoryService.getResourceAsStream(processDefinition.getDeploymentId(), resourceName);
+        byte[] b = new byte[1024];
+        int len = -1;
+        while ((len = resourceAsStream.read(b, 0, 1024)) != -1) {
+            response.getOutputStream().write(b, 0, len);
+        }
+    }
+    /**
+     * 挂起、激活流程实例
+     */
+    //TODO 续改成 Rest方式
+    @RequestMapping(value = "/update/{state}/{processDefinitionId}")
+    public String updateState(@PathVariable("state") String state, @PathVariable("processDefinitionId") String processDefinitionId,
+                              RedirectAttributes redirectAttributes) {
+        if (state.equals("active")) {
+            redirectAttributes.addFlashAttribute("message", "已激活ID为[" + processDefinitionId + "]的流程定义。");
+            repositoryService.activateProcessDefinitionById(processDefinitionId, true, null);
+        } else if (state.equals("suspend")) {
+            repositoryService.suspendProcessDefinitionById(processDefinitionId, true, null);
+            redirectAttributes.addFlashAttribute("message", "已挂起ID为[" + processDefinitionId + "]的流程定义。");
+        }
+        return "redirect:/sys/workflow/showList";
     }
 }

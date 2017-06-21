60a7a395415da7361b4b6003ce5c5d5c74471f68
solomon <ldlqdsdcn@gmail.com>
2017-06-21 16:39:37 +0800

    添加power mock

--- ebase/src/test/java/com/dsdl/eidea/base/test/SimpleMockTest.java
+++ ebase/src/test/java/com/dsdl/eidea/base/test/SimpleMockTest.java
@@ -1,17 +1,19 @@
 package com.dsdl.eidea.base.test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.*;
 
 /**
- * Created by cityre on 2017/6/21.
+ * Created by 刘大磊 on 2017/6/21.
  */
+@Slf4j
 public class SimpleMockTest {
     @Test
     public void testExample()
@@ -23,9 +25,27 @@ public class SimpleMockTest {
         String result = list.get(0);
 
         //验证方法调用(是否调用了get(0))
-        Mockito.verify(list).get(0);
+        verify(list).get(0);
 
         //junit测试
         Assert.assertEquals("helloworld", result);
+    }
+    @Test
+    public void argumentMatcherTest()
+    {
+        List<String> list = mock(List.class);
+
+        when(list.get(anyInt())).thenReturn("hello","world","apple","orange");
+
+        String result = list.get(0)+list.get(0);
+        String result2=list.get(0)+list.get(4);
+        verify(list,times(4)).get(anyInt());
+
+        Assert.assertEquals("helloworld", result);
+        log.warn(result);
+        log.warn("result2="+result2);
+        log.warn(list.get(3));
+        log.warn(list.get(7)+list.get(8));
+        log.warn(list.get(8)+list.get(9));
     }
 }
